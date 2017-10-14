f95a36440b73cf9e4c6c9ad870cbef11d01df926
Rafal Ozog <rafalozog@gmail.com>
2017-10-14 12:47:28 +0000

    Search function and new css style added.

--- data.py
+++ data.py
@@ -1,22 +1,27 @@
 import json
 
 def get_number():
-  with open('data/offers.json') as data_file:
-    data = json.load(data_file)
+  with open('data/offers.json') as data_file0:
+    data0 = json.load(data_file0)
   acc = 0
-  for row in data["offers"]:
+  for row in data0["offers"]:
     acc += 1
   return acc
-
-def read_last():
-  with open('data/offers.json') as data_file:
-    data = json.load(data_file)
-    last_number = get_number() - 1
-    last_offer = data["offers"][last_number]
-  return last_offer
 
 def read_offer( number ):
   with open('data/offers.json') as data_file:
     data = json.load(data_file)
-    offer = data["offers"][number]
+    offer = []
+    attributes = ["desc", "endDate", "endPrice", "id", "seller", "startDate",
+    "startPrice", "title"]
+    for item in attributes:
+      offer.append(data["offers"][number][item])
   return offer
+
+def add_offer ( offer ):
+  with open ('data/offers.json') as data_file:
+    data = json.load(data_file)
+    data['offers'].extend( [offer] )
+  with open('data/offers.json', 'w') as outfile:
+    json.dump(data, outfile, sort_keys=True)
+  return;
--- index.py
+++ index.py
@@ -1,21 +1,50 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, request
 import data
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  acc = data.get_number()
-  offer1 = data.read_offer(acc - 1)
-  offer2 = data.read_offer(acc - 2)
-  offer3 = data.read_offer(56)
-  return render_template('index.html', offer1 = offer1, offer3 = offer3)
+  last_index = data.get_number() - 1
+  fresh_offers = []
+
+  for x in range(10):
+    try:
+      if (last_index-x) >= 0:
+        fresh_offers.append( data.read_offer(last_index-x) )
+    except:
+      print "reading error"
+
+  return render_template('index.html', fresh_offers = fresh_offers)
 
 @app.route("/sell", methods=['POST', 'GET'])
 def sell():
   if request.method == 'POST':
+    id = "AXN000"+str(data.get_number()+1)
+    title = request.form['title']
+    desc = request.form['desc']
+    startDate = datetime.now().strftime("%d.%m.%Y %H:%M")
+    endDate = request.form['endDate']
+    startPrice = request.form['startPrice']
+    endPrice = request.form['endPrice']
+    seller = "Sylvester Omsky"
+    photo = request.files['photo']
+    photo.save('static/uploads/'+id+'.png')
+    offer = {'id': id, 'title': title, 'desc': desc, 'startDate': startDate,
+    'endDate': endDate, 'startPrice':startPrice, 'endPrice':endPrice,
+    'seller':seller}
+    data.add_offer( offer )
+    return "Done"
+  else:
     return render_template('sell.html')
+
+@app.route("/search", methods=['POST', 'GET'])
+def search():
+  if request.method =='POST':
+    print "ok"
+  else:
+    return render_template('search.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
